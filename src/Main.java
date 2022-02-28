2587ebefa8cac5243456de2ee7c7178488fcf151
Todd Raharivelo <t.raharivelo@outlook.fr>
2022-02-28 12:10:30 +0100

    feat: add fuite

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Baignoire baignoire = new Baignoire(1000);
+        Baignoire baignoire = new Baignoire(1000, 10);
         Robinet robinet = new Robinet(baignoire, 50);
 
         robinet.debite();
+        baignoire.fuite();
     }
 }
--- src/classes/Baignoire.java
+++ src/classes/Baignoire.java
@@ -1,13 +1,25 @@
 package classes;
 
+import static util.PrintFormat.FORMAT;
+
 public class Baignoire {
 
     private int volumeMax;
     private int volumeActuel;
+    private int volumeFuite;
 
+    // Baignoire qui ne fuit pas
     public Baignoire(int volumeMax) {
         this.volumeMax = volumeMax;
         this.volumeActuel = 0;
+        this.volumeFuite = 0;
+    }
+
+    // Baignoire qui fuit
+    public Baignoire(int volumeMax, int volumeFuite) {
+        this.volumeMax = volumeMax;
+        this.volumeActuel = 0;
+        this.volumeFuite = volumeFuite;
     }
 
     public int getVolumeMax() {
@@ -20,5 +32,18 @@ public class Baignoire {
 
     public void setVolumeActuel(int volumeActuel) {
         this.volumeActuel = volumeActuel;
+    }
+
+    public int getVolumeFuite() {
+        return volumeFuite;
+    }
+
+    public void fuite() {
+        while (this.volumeActuel > 0) {
+            this.volumeActuel -= this.volumeFuite;
+            String prefix = "Volume baignoire après fuite :";
+            String suffix = " L";
+            System.out.printf(FORMAT, prefix, this.volumeActuel, suffix);
+        }
     }
 }
