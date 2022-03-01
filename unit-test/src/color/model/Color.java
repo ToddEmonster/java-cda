10af36290fcb0c240f1e1b03eafe55297e5da041
Todd Raharivelo <t.raharivelo@outlook.fr>
2022-03-01 14:31:50 +0100

    feat: add test constructor exception

--- unit-test/src/color/model/Color.java
+++ unit-test/src/color/model/Color.java
@@ -6,10 +6,17 @@ public class Color {
     public int green;
     public int blue;
 
-    public Color(int red, int green, int blue) {
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
+    public Color(int red, int green, int blue) throws IllegalArgumentException {
+        if (red < 0 || red > 255 ||
+            green < 0 || green > 255 ||
+            blue < 0 || blue > 255 )
+        {
+            throw new IllegalArgumentException("La valeur d'une couleur doit être comprise entre 0 et 255");
+        } else {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
     }
 
 }
--- unit-test/test/color.model/ColorTest.java
+++ unit-test/test/color.model/ColorTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ColorTest {
 
@@ -21,6 +21,66 @@ public class ColorTest {
         assertEquals(2, color.green, "constructeur incorrect sur green");
         assertEquals(3, color.blue, "constructeur incorrect sur blue");
     }
+
+    @Test
+    public void testConstructorThrowsExceptionArgumentTooLow() {
+        String expectedErrorMessage = "La valeur d'une couleur doit être comprise entre 0 et 255";
+        IllegalArgumentException exceptionRed = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Color(-1, 0, 0),
+                "Exception non lancée pour une valeur de rouge < 0");
+
+        assertTrue(exceptionRed.getMessage().contains(expectedErrorMessage),
+                   "Message d'exception manquant pour une valeur de rouge < 0");
+
+        IllegalArgumentException exceptionGreen = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Color(0, -1, 0),
+                "Exception non lancée pour une valeur de vert < 0");
+
+        assertTrue(exceptionGreen.getMessage().contains(expectedErrorMessage),
+                   "Message d'exception manquant pour une valeur de vert < 0");
+
+
+        IllegalArgumentException exceptionBlue = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Color(0, 0, -1),
+                "Exception non lancée pour une valeur de bleu < 0");
+
+        assertTrue(exceptionBlue.getMessage().contains(expectedErrorMessage),
+                   "Message d'exception manquant pour une valeur de bleu < 0");
+
+    }
+
+    @Test
+    public void testConstructorThrowsExceptionArgumentTooHigh() {
+        String expectedErrorMessage = "La valeur d'une couleur doit être comprise entre 0 et 255";
+        IllegalArgumentException exceptionRed = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Color(256, 0, 0),
+                "Exception non lancée pour une valeur de rouge > 255");
+
+        assertTrue(exceptionRed.getMessage().contains(expectedErrorMessage),
+                   "Message d'exception manquant pour une valeur de rouge > 255");
+
+        IllegalArgumentException exceptionGreen = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Color(0, 256, 0),
+                "Exception non lancée pour une valeur de vert > 255");
+
+        assertTrue(exceptionGreen.getMessage().contains(expectedErrorMessage),
+                   "Message d'exception manquant pour une valeur de vert > 255");
+
+
+        IllegalArgumentException exceptionBlue = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Color(0, 0, 256),
+                "Exception non lancée pour une valeur de bleu > 255");
+
+        assertTrue(exceptionBlue.getMessage().contains(expectedErrorMessage),
+                   "Message d'exception manquant pour une valeur de bleu > 255");
+    }
+
 
     @AfterEach
     public void throwOut() {
