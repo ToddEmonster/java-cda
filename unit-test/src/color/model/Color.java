f7374778c23fa87b7c0f4c6d1b189d1f5c73467d
Todd Raharivelo <t.raharivelo@outlook.fr>
2022-03-01 12:10:11 +0100

    feat: add JUnit dependencies etc, first test failed OK

--- unit-test/src/color/model/Color.java
+++ unit-test/src/color/model/Color.java
@@ -1,4 +1,16 @@
 package color.model;
 
 public class Color {
+
+    private int red;
+    public int green;
+    public int blue;
+
+    public Color(int red, int green, int blue) {
+
+    }
+
+    public int getRed() {
+        return 0;
+    }
 }
--- unit-test/test/color.model/ColorTest.java
+++ unit-test/test/color.model/ColorTest.java
@@ -1,6 +1,15 @@
-package color.model;
+import color.model.Color;
+import org.junit.jupiter.api.Test;
 
-@Test
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ColorTest {
 
+    private Color color;
+
+    @Test
+    public void testGetRed() {
+        Color color = new Color(1, 1, 1);
+        assertEquals(1, color.getRed(), "getRed() est incorrecte");
+    }
 }
