2760f78234b7e827022d4e4c10fce6391c4a8639
Todd Raharivelo <t.raharivelo@outlook.fr>
2022-03-03 11:46:51 +0100

    feat: add sliders and main XML structure

--- gui/src/main/java/com/example/gui/ColorAppController.java
+++ gui/src/main/java/com/example/gui/ColorAppController.java
@@ -1,52 +1,83 @@
 package com.example.gui;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 
 public class ColorAppController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    private HBox hb = new HBox();
 
     @FXML
     private Label redLabel;
+    @FXML
+    private Label greenLabel;
+    @FXML
+    private Label blueLabel;
+    @FXML
+    private Label hexLabel;
 
     @FXML
     private Slider redSlider = new Slider(0, 255, 213);
 
     @FXML
+    private Slider greenSlider = new Slider(0, 255, 141);
+
+    @FXML
+    private Slider blueSlider = new Slider(0, 255, 53);
+
+    @FXML
     private TextField redInput = new TextField();
 
     @FXML
-    private void initialize() {
-        redLabel.setText("Red");
-        redInput.setText(Integer.toString((int) redSlider.getValue()));
+    private TextField greenInput = new TextField();
 
+    @FXML
+    private TextField blueInput = new TextField();
+
+    @FXML
+    private TextField hexInput = new TextField();
+
+    // TODO color square
+
+
+    @FXML
+    private void initialize() {
+        // Labels
+        redLabel.setText("Red");
+        greenLabel.setText("Green");
+        blueLabel.setText("Blue");
+        hexLabel.setText("Hex");
+
+        // Input
+        redInput.setText(Integer.toString((int) redSlider.getValue()));
+        greenInput.setText(Integer.toString((int) greenSlider.getValue()));
+        blueInput.setText(Integer.toString((int) blueSlider.getValue()));
+        hexInput.setText("#TODO");
+
+        // RED slider/input binding
         redSlider.valueProperty().addListener(
                 (observableValue, oldValue, newValue) -> redInput.setText(
                         Integer.toString((int) redSlider.getValue())));
         redInput.textProperty().addListener(
                 (observableText, oldText, newText) -> redSlider.setValue(
                         Double.parseDouble(redInput.textProperty().getValue())));
+
+        // GREEN slider/input binding
+        greenSlider.valueProperty().addListener(
+                (observableValue, oldValue, newValue) -> greenInput.setText(
+                        Integer.toString((int) greenSlider.getValue())));
+        greenInput.textProperty().addListener(
+                (observableText, oldText, newText) -> greenSlider.setValue(
+                        Double.parseDouble(greenInput.textProperty().getValue())));
+
+        // BLUE slider/input binding
+        blueSlider.valueProperty().addListener(
+                (observableValue, oldValue, newValue) -> blueInput.setText(
+                        Integer.toString((int) blueSlider.getValue())));
+        blueInput.textProperty().addListener(
+                (observableText, oldText, newText) -> blueSlider.setValue(
+                        Double.parseDouble(blueInput.textProperty().getValue())));
     }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
-//    @FXML
-//    protected void onSliderChanged() {
-//        int slidervalue = (int) redSlider.getValue();
-//        redInput.setText(Integer.toString(slidervalue));
-//    }
-
-
+    // TODO : if no text in input when focus is out, set to 0
 }
